53393f6a1a33aeb627f96ab0e7eb5819f7b5359f
Nick Rodriguez <nickbenrodriguez@gmail.com>
2018-10-13 00:00:44 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -55,4 +55,5 @@ function removeFromCart(item) {
 
 function placeOrder(cardNumber) {
   // write your code here
+  return `Your total cost is ${total()}, which will be charged to the card ${cardNumber}.`
 }
